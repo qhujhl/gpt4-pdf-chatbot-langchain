81def3c8172eb66651366bb386006dbbdf259163
Qiang Hu <qhu@jhldigital.com>
2024-03-14 13:41:18 +0800

    Update styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,7 +125,7 @@ export default function Home() {
       <Layout>
         <div className="mx-auto flex flex-col gap-4">
           <h1 className="text-2xl font-bold leading-[1.1] tracking-tighter text-center">
-            Chat PDF
+            <span className={styles.jhlgreen}>Chat PDF</span>
           </h1>
           <main className={styles.main}>
             <div className={styles.cloud}>
